7b793ad75b266c20bc1f88d69ec19ce9441a32ab
Sanmit Sahu <sanmitsahu@Sanmits-Air.lan>
2023-01-27 23:47:00 -0800

    Sword fire

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -15,7 +15,28 @@ public class PlayerController : MonoBehaviour
     {
         
     }
+    void OnCollisionEnter(Collision collision)
+    {
+        
+        //Check for a match with the specified name on any GameObject that collides with your GameObject
+        if (collision.gameObject.tag == "BossSword" && SwordSwing.blockon)
+        {
+            //If the GameObject's name matches the one you suggest, output this message in the console
+            StartCoroutine(waitercolor());
+        }
+        else if (collision.gameObject.name == "BossSword" && SwordSwing.blockon)
+        {
+            StartCoroutine(waitercolor());
+        }
 
+    }
+    IEnumerator waitercolor()
+    {
+        swordPrefab.GetComponent<Renderer> ().material.color = Color.yellow;
+        yield return new WaitForSecondsRealtime(5);
+        swordPrefab.GetComponent<Renderer> ().material.color = Color.grey;
+    }
+    
     // Update is called once per frame
     void Update()
     {
@@ -26,19 +47,21 @@ public class PlayerController : MonoBehaviour
         moveDirect.Normalize();
 
         transform.Translate(moveDirect * speed * Time.deltaTime, Space.World);
-
-
-
         if (moveDirect != Vector3.zero)
         {
             //Quaternion toRot = Quaternion.LookRotation(moveDirect, Vector3.up);
             //transform.rotation = Quaternion.RotateTowards(transform.rotation, toRot, turnSpeed * Time.deltaTime);
-            transform.forward = moveDirect;
+            //transform.forward = moveDirect;
         }
 
         if (Input.GetKeyDown(KeyCode.Space) && swordPrefab.GetComponent <SwordSwing>().timer == 0.5f)
         {
             SwordSwing.swung = true;
+        }
+        if ((Input.GetKey(KeyCode.LeftShift) || Input.GetKey(KeyCode.RightShift)) && swordPrefab.GetComponent <SwordSwing>().timer == 0.5f)
+        {
+            SwordSwing.block = true;
+            SwordSwing.blockon = true;
         }
     }
 }
--- Assets/Scripts/SwordSwing.cs
+++ Assets/Scripts/SwordSwing.cs
@@ -10,7 +10,10 @@ public class SwordSwing : MonoBehaviour
     public float swingSpeed = 1000.0f;
     public float timer = 0.5f;
     public static bool swung = false;
+    public static bool block = false;
+    public static bool blockon = false;
     private bool swinging = false;
+    private bool blocking = false;
     private Quaternion originalRot;
     private Vector3 originalPos;
     // Start is called before the first frame update
@@ -44,11 +47,46 @@ public class SwordSwing : MonoBehaviour
             }
             transform.RotateAround(character.transform.position, Vector3.up, -swingSpeed * Time.deltaTime);
         }
+        if (block)
+        {
+            if (!blocking)
+            {
+                transform.localPosition = startPos;
+                transform.localRotation = startRot;
+                blocking = true;
+            }
+
+            timer -= Time.deltaTime;
+            if (timer <= 0.0f)
+            {
+                blocking = false;
+                block = false;
+                timer = 0.5f;
+
+            }
+    
+            StartCoroutine(waiter(originalPos, originalRot));
+            
+        }
         else
         {
             transform.localPosition = originalPos;
             transform.localRotation = originalRot;
         }
+        
+        
+    }
+    IEnumerator waiter(Vector3 startPos, Quaternion startRot)
+    {
+        transform.localRotation = Quaternion.Euler(new Vector3(0,-90,0));
+        yield return new WaitForSecondsRealtime(2);
+        //transform.RotateAround(character.transform.position, Vector3.up, -500.0f * Time.deltaTime);
+        //UnityEngine.Debug.Log("start wait");
+        //UnityEngine.Debug.Log("done wait");
+        blockon = false;
+        transform.localPosition = startPos;
+        transform.localRotation = startRot;
+
     }
 }
 
