f2bb3f5ad6abe35caf16f6149261243c3f720d10
jitbanerjee4 <jitbanerjee4@gmail.com>
2023-02-20 01:47:12 -0800

    Modified boss movement glitch

--- Assets/Scripts/GameOverManager.cs
+++ Assets/Scripts/GameOverManager.cs
@@ -16,6 +16,7 @@ public class GameOverManager : MonoBehaviour
         Time.timeScale = 1;
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
         PlayerController.swung = false;
+        SwitchOn.on = false;
         gameOverScreen.SetActive(false);
     }
 }
