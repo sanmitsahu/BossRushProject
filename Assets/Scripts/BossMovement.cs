cc4c474f15cd2d53603b4df9a4dad7107414c0ef
ejyiusc <94198618+ejyiusc@users.noreply.github.com>
2023-04-11 10:24:54 -0700

    Added boss movement to level 7

---
+++ Assets/Scripts/BossMovement.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BossMovement : MonoBehaviour
+{
+
+    private float updateRate = 2.0f;
+    private float updateTimer;
+    GameObject player;
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GameObject.FindWithTag("Player");
+        updateTimer = updateRate;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        updateTimer -= Time.deltaTime;
+        if (updateTimer <= 0)
+        {
+            gameObject.GetComponent<Rigidbody>().velocity = (gameObject.transform.position - player.transform.position).normalized * 0.2f;
+            updateTimer = updateRate;
+
+        }
+        
+
+    }
+}
